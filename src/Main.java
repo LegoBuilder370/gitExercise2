102e9f8a8c3c12b366bad7630bfea06266588b23
LegoBuilder370 <cjkobulnicky@gmail.com>
2025-02-17 14:22:00 -0500

    try 2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Test 2 is successful (I think)");
+        System.out.println("it did work");
     }
 }
